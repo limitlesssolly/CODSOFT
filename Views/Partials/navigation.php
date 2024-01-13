3f408c494ebb4d8988d7e98207fe2d6f80f57ba1
shahd ahmed <shahdahmed7203@gmail.com>
2024-01-13 20:14:50 +0200

    navigation done-ish

---
+++ Views/Partials/navigation.php
@@ -0,0 +1,28 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../../Public/CSS/navbar.css">
+
+    <title>Navigation</title>
+</head>
+<body>
+    <div class="navs">
+
+            <img class="iconic" src="../../Public/Images/sols-transparent.png">
+
+            <a class="named"><span>Sols </span> Productions</a>
+
+        <div class="navi-items">
+            <ul>
+                <li><a href="">Skills</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Achievments</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+
+    </div>
+</body>
+</html>
