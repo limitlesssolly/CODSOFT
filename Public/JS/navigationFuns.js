fae7fd5bb1114d1dc3fdc111a1047938d1590239
shahd ahmed <shahdahmed7203@gmail.com>
2024-01-14 18:46:06 +0200

    responsiveness fixed 4 reals

--- Public/JS/navigationFuns.js
+++ Public/JS/navigationFuns.js
@@ -1,16 +1,35 @@
 var items = document.getElementById("navi-items");
+var burgerDisplay = document.getElementById("burger");
 
 function navigational() {
-    if (items.className === "navi-items") {
-        open()
-    } else {
-        close()
+    var computedBurger = window.getComputedStyle(burgerDisplay);
+    var burgerValue = computedBurger.getPropertyValue('display');
+
+    if (window.innerWidth <= 600 && burgerValue === "flex") {
+        if (items.className === "navi-items") {
+            open();
+        } else {
+            close();
+        }
     }
 }
 
 function open() {
     items.className += "-responsive";
 }
+
 function close() {
     items.className = "navi-items";
 }
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth > 600) {
+        items.className = "navi-items";
+    }
+});
+
+window.addEventListener('load', function () {
+    if (window.innerWidth > 600) {
+        items.className = "navi-items";
+    }
+});
