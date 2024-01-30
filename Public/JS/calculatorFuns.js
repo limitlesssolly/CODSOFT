77ac5021ad5c9ecf47a0c164a37dc4a569e8688f
shahd ahmed <shahdahmed7203@gmail.com>
2024-01-30 14:20:37 +0200

    working js without keybaord usage

--- Public/JS/calculatorFuns.js
+++ Public/JS/calculatorFuns.js
@@ -1,3 +1,5 @@
+
+
 function displays(value){
     document.getElementById("displayer").value += value;
 }
@@ -7,9 +9,31 @@ function delDisplayed(){
 }
 
 function delOneDisplayed(){
-    document.getElementById("displayer").value = document.getElementById("displayer").value.toString().slice(0,-1)
+    document.getElementById("displayer").value = document.getElementById("displayer").value.toString().slice(0,-1);
 }
-
+onclick="displayer.value = eval(displayer.value)"
 function calculate(){
-
+    document.getElementById("displayer").value = eval(document.getElementById("displayer").value);
 }
+
+// for keyboard typed vals
+function listener(event) { 
+    if (event.key == '0' || event.key == '1' 
+        || event.key == '2' || event.key == '3' 
+        || event.key == '4' || event.key == '5' 
+        || event.key == '6' || event.key == '7' 
+        || event.key == '8' || event.key == '9' 
+        || event.key == '+' || event.key == '-' 
+        || event.key == '*' || event.key == '/') 
+        document.getElementById("displayer").value += event.key; 
+} 
+
+var cal = document.getElementsByClassName("operational"); 
+cal.onkeyup = function (event) { 
+    if (event.keyCode === 13) { 
+        console.log("Enter"); 
+        let x = document.getElementById("displayer").value 
+        console.log(x); 
+        solve(); 
+    } 
+} 
