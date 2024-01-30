e4a0e7d97fb91006f6bf3c1074cc95cabe1f9eab
shahd ahmed <shahdahmed7203@gmail.com>
2024-01-30 14:28:27 +0200

    correctly done now

--- Public/JS/calculatorFuns.js
+++ Public/JS/calculatorFuns.js
@@ -1,5 +1,3 @@
-
-
 function displays(value){
     document.getElementById("displayer").value += value;
 }
@@ -16,24 +14,16 @@ function calculate(){
     document.getElementById("displayer").value = eval(document.getElementById("displayer").value);
 }
 
-// for keyboard typed vals
-function listener(event) { 
-    if (event.key == '0' || event.key == '1' 
-        || event.key == '2' || event.key == '3' 
-        || event.key == '4' || event.key == '5' 
-        || event.key == '6' || event.key == '7' 
-        || event.key == '8' || event.key == '9' 
-        || event.key == '+' || event.key == '-' 
-        || event.key == '*' || event.key == '/') 
+document.addEventListener("keydown", function(event) { 
+    if ((event.key >= '0' && event.key <= '9') || event.key == '+' || event.key == '-' || event.key == '*' || event.key == '/') {
         document.getElementById("displayer").value += event.key; 
-} 
+        event.preventDefault();
+    }
+});
 
-var cal = document.getElementsByClassName("operational"); 
-cal.onkeyup = function (event) { 
-    if (event.keyCode === 13) { 
-        console.log("Enter"); 
-        let x = document.getElementById("displayer").value 
-        console.log(x); 
-        solve(); 
+document.addEventListener("keydown", function(event) { 
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        calculate(); 
     } 
-} 
+});
