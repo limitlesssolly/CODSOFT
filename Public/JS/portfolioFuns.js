d1dfcf7f50325fc9070ad5579fbe652cb901af8e
shahd ahmed <shahdahmed7203@gmail.com>
2024-01-31 20:36:31 +0200

    popups workin

--- Public/JS/portfolioFuns.js
+++ Public/JS/portfolioFuns.js
@@ -1,47 +1,90 @@
-let next = document.getElementById('next')
-let then = document.getElementById('then')
-let slider = document.querySelector('.slidey')
-let projects = document.querySelector('.slidey .projects')
-let smolprojects = document.querySelector('.slidey .smolprojects')
+document.addEventListener('DOMContentLoaded', function () {
 
-let running = 2000;
-let autoTime = 7000;
-let timeOut;
-let autoTimeOut;
+    let next = document.getElementById('next')
+    let then = document.getElementById('then')
+    let slider = document.querySelector('.slidey')
+    let projects = document.querySelector('.slidey .projects')
+    let smolprojects = document.querySelector('.slidey .smolprojects')
 
-next.onclick = function () {
-    showslidey('next');
-}
+    let running = 2000;
+    let autoTime = 7000;
+    let timeOut;
+    let autoTimeOut;
 
-then.onclick = function () {
-    showslidey('then');
-}
-
-function showslidey(variable) {
-    let allProjects = document.querySelectorAll('.slidey .projects .project')
-    let allSmolProjects = document.querySelectorAll('.slidey .smolprojects .smolproject')
-
-    if (variable === 'next') {
-        projects.appendChild(allProjects[0]);
-        smolprojects.appendChild(allSmolProjects[0]);
-        slider.classList.add('next');
+    next.onclick = function () {
+        showslidey('next');
     }
 
-    else {
-        let lastone = allProjects.length - 1;
-        projects.prepend(allProjects[lastone]);
-        smolprojects.prepend(allSmolProjects[lastone]);
-        slider.classList.add('then');
+    then.onclick = function () {
+        showslidey('then');
     }
 
-    clearTimeout(timeOut);
-    timeOut = setTimeout(() => {
-        slider.classList.remove('next');
-        slider.classList.remove('then');
-    }, running)
+    function showslidey(variable) {
+        let allProjects = document.querySelectorAll('.slidey .projects .project')
+        let allSmolProjects = document.querySelectorAll('.slidey .smolprojects .smolproject')
 
-    clearTimeout(autoTimeOut);
-    autoTimeOut = setTimeout(() => {
-        next.click();
-    }, autoTime);
-}
+        if (variable === 'next') {
+            projects.appendChild(allProjects[0]);
+            smolprojects.appendChild(allSmolProjects[0]);
+            slider.classList.add('next');
+        }
+
+        else {
+            let lastone = allProjects.length - 1;
+            projects.prepend(allProjects[lastone]);
+            smolprojects.prepend(allSmolProjects[lastone]);
+            slider.classList.add('then');
+        }
+
+        clearTimeout(timeOut);
+        timeOut = setTimeout(() => {
+            slider.classList.remove('next');
+            slider.classList.remove('then');
+        }, running)
+
+        clearTimeout(autoTimeOut);
+        autoTimeOut = setTimeout(() => {
+            next.click();
+        }, autoTime);
+    }
+
+    let previews = document.querySelectorAll('.slidey .projects .project .preview button');
+    let closes = '<span class="close">&times;</span>';
+    let body = document.body;
+
+    previews.forEach(button => {
+        button.addEventListener('click', function () {
+            let projectDiv = button.closest('.project');
+            let sources = projectDiv.querySelector('img').getAttribute('src');
+            openPopup(sources);
+        });
+    });
+
+    function openPopup(sources) {
+        let popups = `
+            <div class="popups">
+                ${closes}
+                <img src="${sources}">
+            </div>
+        `;
+
+        body.insertAdjacentHTML('beforeend', popups);
+
+        let popupper = document.querySelector('.popups');
+        let closeButton = popupper.querySelector('.close');
+
+        closeButton.addEventListener('click', function () {
+            closePopup();
+        });
+
+        body.style.overflow = 'hidden';
+    }
+
+    function closePopup() {
+        let popups = document.querySelector('.popups');
+        if (popups) {
+            popups.remove();
+            body.style.overflow = '';
+        }
+    }
+});
