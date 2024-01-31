10cbf31f08b13f1a4fbe1ddd2741518ae8f99894
shahd ahmed <shahdahmed7203@gmail.com>
2024-01-31 17:32:34 +0200

    image slider workingg

---
+++ Public/JS/portfolioFuns.js
@@ -0,0 +1,47 @@
+let next = document.getElementById('next')
+let then = document.getElementById('then')
+let slider = document.querySelector('.slidey')
+let projects = document.querySelector('.slidey .projects')
+let smolprojects = document.querySelector('.slidey .smolprojects')
+
+let running = 2000;
+let autoTime = 7000;
+let timeOut;
+let autoTimeOut;
+
+next.onclick = function () {
+    showslidey('next');
+}
+
+then.onclick = function () {
+    showslidey('then');
+}
+
+function showslidey(variable) {
+    let allProjects = document.querySelectorAll('.slidey .projects .project')
+    let allSmolProjects = document.querySelectorAll('.slidey .smolprojects .smolproject')
+
+    if (variable === 'next') {
+        projects.appendChild(allProjects[0]);
+        smolprojects.appendChild(allSmolProjects[0]);
+        slider.classList.add('next');
+    }
+
+    else {
+        let lastone = allProjects.length - 1;
+        projects.prepend(allProjects[lastone]);
+        smolprojects.prepend(allSmolProjects[lastone]);
+        slider.classList.add('then');
+    }
+
+    clearTimeout(timeOut);
+    timeOut = setTimeout(() => {
+        slider.classList.remove('next');
+        slider.classList.remove('then');
+    }, running)
+
+    clearTimeout(autoTimeOut);
+    autoTimeOut = setTimeout(() => {
+        next.click();
+    }, autoTime);
+}
